67a65fc0ab09768080f5ddf5b08fbdb3fa8b0d5f
Marcin Białoń <mbialon@spacelift.io>
2026-03-12 14:25:39 +0100

    write file into a subdirectory

--- stub-git.sh
+++ stub-git.sh
@@ -7,7 +7,17 @@ case "$1" in
     # Last argument is the destination directory
     while [ $# -gt 1 ]; do shift; done
     dest="$1"
+
     cat > "$dest/demo.tf" <<'EOF'
+resource "null_resource" "demo" {}
+
+output "demo" {
+  value = null_resource.demo.id
+}
+EOF
+
+    mkdir -p "$dest/stack"
+    cat > "$dest/stack/demo.tf" <<'EOF'
 resource "null_resource" "demo" {}
 
 output "demo" {
