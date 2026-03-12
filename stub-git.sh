596f042865fe0738ccc54b6112d037915eb6c707
Marcin Białoń <mbialon@spacelift.io>
2026-03-12 11:06:58 +0100

    stub

---
+++ stub-git.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+case "$1" in
+  clone)
+    shift
+    # Last argument is the destination directory
+    while [ $# -gt 1 ]; do shift; done
+    dest="$1"
+    cat > "$dest/demo.tf" <<'EOF'
+resource "null_resource" "demo" {}
+
+output "demo" {
+  value = null_resource.demo.id
+}
+EOF
+    ;;
+esac
